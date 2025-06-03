e162deda0bdbbf72a24d0157f04dd646e6b1f116
dammaitre <damien.maitre@live.fr>
2025-06-03 16:07:12 +0200

    hello

--- tools_electrovanne.h
+++ tools_electrovanne.h
@@ -1,7 +1,7 @@
 #ifndef TOOLS_electrovanne_H
 #define TOOLS_electrovanne_H
 
-const int RELAY_PIN = 4; // pin de commande du relai de la vanne
+const int RELAY_PIN = 6; // pin de commande du relai de la vanne
 extern bool bool_ballast;
 
 void Vanne(bool etat);
